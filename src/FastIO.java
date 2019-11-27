3c6cd8c2392e60ebcda8791709063157e495103a
yoonseop <sys09270883@gmail.com>
2019-11-27 11:55:10 +0900

    Initial commit

---
+++ src/FastIO.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.util.*;
+
+class FastIO {
+    static BufferedReader br;
+    static BufferedWriter bw;
+    static StringTokenizer st;
+
+    FastIO() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    }
+
+    String next() {
+        while (st == null || !st.hasMoreTokens()) {
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return st.nextToken();
+    }
+
+    int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+        return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+        return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+        String str = null;
+        try {
+            str = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return str;
+    }
+
+    void write(double d) throws IOException {
+        bw.write(String.valueOf(d));
+        close();
+    }
+
+    void write(int i) throws IOException {
+        bw.write(String.valueOf(i));
+        close();
+    }
+
+    void write(long l) throws IOException {
+        bw.write(String.valueOf(l));
+        close();
+    }
+
+    void write(StringBuilder sb) throws IOException {
+        bw.write(sb.toString().trim());
+        close();
+    }
+
+    void write(String str) throws IOException {
+        bw.write(str.trim());
+        close();
+    }
+
+    void close() throws IOException {
+        bw.close();
+        br.close();
+    }
+}
